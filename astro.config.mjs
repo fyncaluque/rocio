8736ff465e1ef9aaf9e57d77eb13e710f86294ba
Paul M. Yncaluque <yncaluquedev@gmail.com>
2023-11-07 18:10:27 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   //   site: "https://fyncaluque.github.io",
-  //   base: "/rocio",
+  base: "/rocio",
 });
