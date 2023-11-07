2c6edd79e73d8215f5767d45258d9004a7793cc6
Paul M. Yncaluque <yncaluquedev@gmail.com>
2023-11-07 12:47:02 -0500

    update site and base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://marcoamd.github.io",
-  base: "/RocioDiazZavala",
+  site: "https://fyncaluque.github.io",
+  base: "/rocio",
 });
